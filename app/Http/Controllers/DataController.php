b75b532ba3f570d39113f37bb8e772c72c6d0b59
Semih-Sinani <semihsinaniofficial@gmail.com>
2023-06-27 01:21:28 +0200

    Completed DataController Side

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class DataController extends Controller
 {
-    //
+    public function Data ($data) {
+        if ($data == 1) {
+            $data = [
+                'page_title' => 'Hello World',
+                'Name' => 'Jason',
+                'ID' => '5254252',
+            ];
+            return view ('Data.app');
+        }
+    }
 }
